6f99d33dcc0dcd2f0b5080d3c4993672481e37bc
harryjung0330 <harryjung0330@gmail.com>
2022-02-13 14:44:31 +0900

    commit

---
+++ lib/area.dart
@@ -0,0 +1,10 @@
+library area;
+
+import 'package:intl/intl.dart';
+
+part 'rectangle.dart';
+part 'triangle.dart';
+
+
+
+
---
+++ lib/triangle.dart
@@ -0,0 +1,9 @@
+part of area;
+
+String calculateAreaTriangle(double width, double height)
+{
+  double result = width * height / 2;
+  final formatter = NumberFormat("#.####");
+
+  return formatter.format(result);
+}
